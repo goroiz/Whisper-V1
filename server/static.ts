467bbe72b128b1e85e1b04d238dae01f49d06fea
goro <ijamullet@outlook.com>
2025-12-22 13:28:18 +0700

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -1,9 +1,15 @@
 import express, { type Express } from "express";
 import fs from "fs";
-import path from "path";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+// Bikin __dirname secara manual untuk ES Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export function serveStatic(app: Express) {
   const distPath = path.resolve(__dirname, "public");
+  
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
